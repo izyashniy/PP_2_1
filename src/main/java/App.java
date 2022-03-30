181ef89f86d1d8be905aa66feb9234d60cf3be4a
Admin <leytmanivan@gmail.com>
2022-03-30 15:39:39 +0700

    fix

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld firstBean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat thirdBean = (Cat) applicationContext.getBean("cat");
+        Cat fourthBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(firstBean == secondBean);
+        System.out.println(thirdBean == fourthBean);
     }
 }
